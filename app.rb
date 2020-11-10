c25af2fa016b1965a76a17e87cafdb3d707383a7
Sandesh Karki <karkisandesh@hotmail.com>
2020-11-10 04:48:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do 
+  get '/name' do 
     "My name is "
+  end
+  
+  get '/hometown' do 
+    "My hometown is "
+  end
+  
+  get '/favorite-song' do 
+    "My favorite-song is "
   end
   
 end
